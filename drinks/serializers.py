521e1fb520d31650ef2c789e382b0844514a63f4
essen900718 <essen900718@gmail.com>
2022-05-23 16:34:21 +0800

    first commit

---
+++ drinks/serializers.py
@@ -0,0 +1,10 @@
+from itsdangerous import Serializer
+
+
+from rest_framework import serializers
+from .models import Drink
+
+class DrinkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Drink
+        fields = ['id', 'name', 'description']
